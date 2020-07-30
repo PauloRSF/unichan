b7a7f5908325da0680297e5f063354c84609d16f
Paulo Renato <paulorsouzaf@gmail.com>
2020-07-29 23:05:32 -0300

    Create api to get posts data from the imageboard

---
+++ lib/services/API.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import '../models/Post.dart';
+
+class API {
+  Future<List<Post>> getThreadsOps() async {
+    Response response = await get('https://55chan.org/b/catalog.json');
+    var data = json.decode(response.body);
+
+    List<Post> posts = new List<Post>();
+    for (var i = 0; i < data.length; i++) {
+      for (var j = 0; j < data[i]['threads'].length; j++) {
+        var post = Post.fromJson(data[i]['threads'][j]);
+        post.isOp = true;
+        posts.add(post);
+      }
+    }
+
+    return posts;
+  }
+
+  Future<List<Post>> getFullThread(no) async {
+    Response response = await get('https://55chan.org/b/res/${no}.json');
+    var data = json.decode(response.body);
+
+    if (data.containsKey('posts')) {
+      List<Post> posts = List<Post>();
+      for (var i = 0; i < data['posts'].length; i++) {
+        var post = Post.fromJson(data['posts'][i]);
+        post.isOp = (i == 0);
+        posts.add(post);
+      }
+      return posts;
+    }
+
+  }
+}
