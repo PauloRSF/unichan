88280c58ac2941270faa071cf50ca6f5bff47adc
Paulo Renato <paulorsouzaf@gmail.com>
2020-08-08 16:04:54 -0300

    Change thread screen to stateless and plug state management to it

--- lib/screens/Thread.dart
+++ lib/screens/Thread.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:provider/provider.dart';
+import '../notifiers/ThreadsNotifier.dart';
 import '../services/API.dart';
 import '../models/Post.dart';
 import '../components/PostCard.dart';
 import '../utils/post_utils.dart'; 
 
-class Thread extends StatefulWidget {
-  int no;
-
-  Thread(this.no);
-
-  @override
-  _ThreadState createState() => _ThreadState();
-}
-
-class _ThreadState extends State<Thread> {
+class Thread extends StatelessWidget {
   int no;
   SortMethod sortMethod = SortMethod.oldest;
   List<Post> posts;
@@ -23,14 +16,7 @@ class _ThreadState extends State<Thread> {
     'Antigos': SortMethod.oldest
   };
 
-  @override
-  void initState() {
-    super.initState();
-    setPostList();
-    //setState(() {
-    //  no = widget.no;
-    //});
-  }
+  Thread(this.no);
 
   Future<List<Post>> getThreadPosts() async {
     var thread = await API().getFullThread(no);
@@ -40,19 +26,8 @@ class _ThreadState extends State<Thread> {
     return Future(() => thread);
   }
 
-  Future<List<Post>> setPostList() async {
-    var threadFuture = getThreadPosts();
-    var thread = await threadFuture;
-    setState(() {
-      posts = thread;
-    });
-
-    return threadFuture;
-  }
-
   @override
   Widget build(BuildContext context) {
-    no = widget.no;
     return Scaffold(
       appBar: AppBar(
         title: Text('Thread #${no}'),
@@ -62,23 +37,36 @@ class _ThreadState extends State<Thread> {
         future: getThreadPosts(),
         builder: (context, AsyncSnapshot snapshot){
           if(snapshot.hasData) {
-            return RefreshIndicator(
-              onRefresh: () => setPostList(),
-              child: Scrollbar(
-                child: ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext ctxt, int index) {
-                    if (index == 0){
-                      return Column(
-                        children: <Widget>[
-                          PostCard(post: snapshot.data[index]),
-                          _commentsOptions(snapshot.data.length),
-                        ],
-                      );
-                    }
-                    return PostCard(post: snapshot.data[index]);
+            return ChangeNotifierProvider(
+              create: (context) => ThreadsNotifier(),
+              child: Consumer<ThreadsNotifier>(
+                builder: (context, thread, child) {
+                  if(thread.posts.isEmpty) {
+                    thread.addAll(snapshot.data);
                   }
-                ),
+                  return RefreshIndicator(
+                    onRefresh: () async {
+                      thread.removeAll();
+                      return thread.addAll(await getThreadPosts());
+                    },
+                    child: Scrollbar(
+                      child: ListView.builder(
+                        itemCount: thread.posts.length,
+                        itemBuilder: (BuildContext ctxt, int index) {
+                          if (index == 0) {
+                            return Column(
+                              children: <Widget>[
+                                PostCard(post: thread.posts[index]),
+                                _commentsOptions(context, thread, thread.posts.length),
+                              ],
+                            );
+                          }
+                          return PostCard(post: thread.posts[index]);
+                        }
+                      ),
+                    ),
+                  );
+                }
               ),
             );
           } else {
@@ -92,7 +80,7 @@ class _ThreadState extends State<Thread> {
     );
   }
 
-  Widget _commentsOptions(int postCount) {
+  Widget _commentsOptions(BuildContext context, ThreadsNotifier thread, int postCount) {
     return Container(
       padding: const EdgeInsets.all(12.0),
       child: Row(
@@ -107,12 +95,8 @@ class _ThreadState extends State<Thread> {
           ),
           GestureDetector(
             onTap: () async {
-              var sort = await _sortMethodDialog();
-              if(sort != null) {
-                setState(() {
-                  sortMethod = sort;
-                });
-              }
+              var sort = await _sortMethodDialog(context);
+              thread.sort(sort);
             },
             child: Icon(
               Icons.sort,
@@ -124,7 +108,7 @@ class _ThreadState extends State<Thread> {
     );
   }
 
-  Future<SortMethod> _sortMethodDialog() {
+  Future<SortMethod> _sortMethodDialog(BuildContext context) {
     return showDialog(
       context: context,
       builder: (_) => new AlertDialog(
@@ -140,9 +124,7 @@ class _ThreadState extends State<Thread> {
                 value: sortMethods.values.elementAt(i),
                 groupValue: sortMethod,
                 onChanged: (SortMethod value) {
-                  setState(() {
-                    Navigator.of(context).pop(value);
-                  });
+                  Navigator.of(context).pop(value);
                 },
               ),
             ),
